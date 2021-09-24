c12311ed592de3741cab612439e904e02d8ed385
Dudrie <Dudrie@gmx.de>
2021-09-24 19:20:35 +0200

    Add files for exercise sheets.

---
+++ src/main/kotlin/03_Grosses_Feld.kt
@@ -0,0 +1,12 @@
+import de.github.dudrie.hamster.imperative.de.laufe
+import de.github.dudrie.hamster.imperative.de.starteSpiel
+
+fun main() {
+    // Starte das Spiel (für die Aufgabe)
+    starteSpielGrossesFeld()
+
+    // Implementiere die Lösung
+
+
+    // Stoppe das Spiel
+}
---
+++ src/main/kotlin/Spiele.kt
@@ -0,0 +1,5 @@
+import de.github.dudrie.hamster.imperative.de.starteSpiel
+
+fun starteSpielGrossesFeld() {
+    starteSpiel("territories/feld_mit_koernern.json")
+}
